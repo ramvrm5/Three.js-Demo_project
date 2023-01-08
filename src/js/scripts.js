8ffc697c54eb78c7014ded387f00f39f5897a02f
ram verma <42866242+ramvrm5@users.noreply.github.com>
2023-01-08 12:32:30 +0530

    file changes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -51,6 +51,7 @@ renderer.setAnimationLoop(animate); */
 const scene = new THREE.Scene();
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+
 scene.add(ambientLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
